f9c77d027f6dd524a5cb4abaabfb2cb8904bf419
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-12-23 16:38:00 +0300

    dependencies and docker

--- ClientService/server.js
+++ ClientService/server.js
@@ -1,2 +1,42 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { connectMongoDB, disconnectMongo } = require('./config/dbConnection');
+
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+
+// Middleware
+app.use(express.json()); // To parse incoming JSON requests
+
+
+// Functions
+const startServer = async () => {
+    try {
+        const isConnected = await connectMongoDB(process.env.MONGO_URI);
+        if(isConnected){
+
+            //app.use('/api/v1/clients', userRoutes);
+            app.listen(port, () => {
+                console.log(`Server is running on port ${port}`);
+            });
+        }
+        else{
+            console.log("Error connecting to MongoDB");
+        }
+    } catch (err) {
+        console.log("Server could not start: ",err);
+    }
+};
+
+// Graceful shutdown
+process.on('SIGINT', async () => {
+    await disconnectMongo();
+    process.exit(0);
+});
+
+// Start the server
+startServer();
