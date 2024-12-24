a4af9de5a8090892a28cc88ebcc6000293ed65e8
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-12-24 15:49:31 +0300

    client gateway implemented

--- Gateway/routes/client.routes.js
+++ Gateway/routes/client.routes.js
@@ -1,11 +1,18 @@
 // routes/clientRoutes.js
 const express = require('express');
-const { createNote } = require('../services/client.service');  // Import ClientService functions
+const { createNote, updateNote ,getClients ,getNotesOfClient, createClient,updateClient} = require('../services/client.service');  // Import ClientService functions
 
 const router = express.Router();
 
+
+
 // Protected routes (requires token)
+router.get('/api/v1/clients', getClients);
+router.get('/api/v1/clients/:clientId/notes', getNotesOfClient);
 router.post('/api/v1/notes', createNote);
+router.post('/api/v1/clients', createClient);
+router.put('/api/v1/clients/:clientId', updateClient);
+router.put('/api/v1/notes/:noteId', updateNote);
 
 
 module.exports = router;
