80ff54f3b78349d69a469f0740d16929ec95c3a5
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-12-24 16:07:01 +0300

    apis added

--- Gateway/routes/sales.routes.js
+++ Gateway/routes/sales.routes.js
@@ -1,11 +1,16 @@
 // routes/saleRoutes.js
 const express = require('express');
-const { createSale } = require('../services/sales.service');  // Import SaleService functions
+const { getSales,getSaleHistory,createSale , updateSale, addSaleNote,editSaleNote} = require('../services/sales.service');  // Import SaleService functions
 
 const router = express.Router();
 
 // Protected routes (requires token)
+router.get('/api/v1/sales', getSales);
+router.get('/api/v1/sales/:saleId/history', getSaleHistory);
 router.post('/api/v1/sales', createSale);
+router.post('/api/v1/sales/:saleId/notes', addSaleNote);
+router.put('/api/v1/sales/:saleId', updateSale);
+router.put('/api/v1/sales/:saleId/notes/:noteId', editSaleNote);
 
 
 module.exports = router;
--- Gateway/services/sales.service.js
+++ Gateway/services/sales.service.js
@@ -3,13 +3,41 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { SALE_SERVICE_URL } = process.env;
+const { SALE_SERVICE_URL, GATEWAY_SECRET_KEY } = process.env;
 
-// Example: Forward create sale request to SaleService
+
+/**
+ * Routing for getting sales api
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getSales = async (req, res) => {
+  try {
+    const response = await axios.get(`${SALE_SERVICE_URL}/api/v1/sales`, {
+      headers: {
+        Authorization: `Bearer ${req.headers['authorization'].split(' ')[1]}`,
+        'x-gateway-secret': GATEWAY_SECRET_KEY
+      }
+    });
+    res.json(response.data);  // Return the response from SaleService
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error getting sales' });
+  }
+}
+
+/**
+ * Routing for creating a sale api
+ * @param {*} req 
+ * @param {*} res 
+ */
 const createSale = async (req, res) => {
   try {
     const response = await axios.post(`${SALE_SERVICE_URL}/api/v1/sales`, req.body, {
-      headers: { Authorization: `Bearer ${req.headers['authorization'].split(' ')[1]}` }
+      headers: {
+        Authorization: `Bearer ${req.headers['authorization'].split(' ')[1]}`,
+        'x-gateway-secret': GATEWAY_SECRET_KEY
+      }
     });
     res.json(response.data);  // Return the response from SaleService
   } catch (error) {
@@ -17,4 +45,82 @@ const createSale = async (req, res) => {
   }
 };
 
-module.exports = { createSale };
+/**
+ * Routing for updating a sale api
+ * @param {*} req 
+ * @param {*} res 
+ */
+const updateSale = async (req, res) => {
+  try {
+    const response = await axios.put(`${SALE_SERVICE_URL}/api/v1/sales/${req.params.id}`, req.body, {
+      headers: {
+        Authorization: `Bearer ${req.headers['authorization'].split(' ')[1]}`,
+        'x-gateway-secret': GATEWAY_SECRET_KEY
+      }
+    });
+    res.json(response.data);  // Return the response from SaleService
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating sale' });
+  }
+};
+
+/**
+ * Routing for adding a note to a sale api
+ * @param {*} req 
+ * @param {*} res 
+ */
+const addSaleNote = async (req, res) => {
+  try {
+    const response = await axios.post(`${SALE_SERVICE_URL}/api/v1/sales/${req.params.id}/notes`, req.body, {
+      headers: {
+        Authorization: `Bearer ${req.headers['authorization'].split(' ')[1]}`,
+        'x-gateway-secret': GATEWAY_SECRET_KEY
+      }
+    });
+    res.json(response.data);  // Return the response from SaleService
+  } catch (error) {
+    res.status(500).json({ message: 'Error adding note to sale' });
+  }
+};
+
+/**
+ * Routing for editing a note to a sale api
+ * @param {*} req 
+ * @param {*} res 
+ */
+const editSaleNote = async (req, res) => {
+  try {
+    const response = await axios.put(`${SALE_SERVICE_URL}/api/v1/sales/${req.params.id}/notes/${req.params.noteId}`, req.body, {
+      headers: {
+        Authorization: `Bearer ${req.headers['authorization'].split(' ')[1]}`,
+        'x-gateway-secret': GATEWAY_SECRET_KEY
+      }
+    });
+    res.json(response.data);  // Return the response from SaleService
+  } catch (error) {
+    res.status(500).json({ message: 'Error editing note' });
+  }
+}
+
+/**
+ * Routing for API Sales History
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getSaleHistory = async (req, res) => {
+  try {
+    const response = await axios.get(`${SALE_SERVICE_URL}/api/v1/sales/${req.params.id}/history`, {
+      headers: {
+        Authorization: `Bearer ${req.headers['authorization'].split(' ')[1]}`,
+        'x-gateway-secret': GATEWAY_SECRET_KEY
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: 'Error getting sale history' });
+  }
+};
+
+
+module.exports = { getSales, getSaleHistory, createSale, updateSale, addSaleNote, editSaleNote };
