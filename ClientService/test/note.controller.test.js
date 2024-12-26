c9e0569aec8ab419ed5c90ced4a3b01c148a7e62
Mehmet Ege Acican <mehmet.ege.acican@gmail.com>
2024-12-26 13:34:04 +0300

    note creation tests added

--- ClientService/test/note.controller.test.js
+++ ClientService/test/note.controller.test.js
@@ -47,10 +47,48 @@ describe('note controller tests', () => {
             });
         });
         test('returns 400 error if missing required fields', async () => {
-
+            // Given
+            req = {
+                body: { title: 'New Note', content: 'New Content' }, // ClientId is missing
+            };
+            res = {
+                status: sinon.stub().returnsThis(),
+                json: sinon.stub(),
+            };
+            const mockNewNote = {
+                title: 'New Note',
+                content: 'New Content',
+                clientId: 'client123',
+                _id: new ObjectId('676d28be7d836af854cb9a55'), // Mock _id value
+            };
+            const saveStub = sinon.stub(Note.prototype, 'save');
+            saveStub.resolves(mockNewNote);
+            // When
+            await createNewNote(req, res);
+            // Then
+            sinon.assert.notCalled(saveStub);
+            sinon.assert.calledWith(res.status, 400);
+            sinon.assert.calledWith(res.json, { message: 'All fields are required' });
         });
         test('server error occurs, returns 500', async () => {
-
+            // Given
+            const req = {
+                body: { title: 'New Note', content: 'New Content', clientId: 'client123' },
+            };
+            const res = {
+                status: sinon.stub().returnsThis(),
+                json: sinon.stub(),
+            };
+            const consoleErrorStub = sinon.stub(console, 'error');
+            const saveStub = sinon.stub(Note.prototype, 'save');
+            saveStub.rejects(new Error('Database error'));
+            // When
+            await createNewNote(req, res);
+            // Then
+            sinon.assert.calledWith(res.status, 500);
+            sinon.assert.calledWith(res.json, { message: 'Internal Server Error' });
+            sinon.assert.calledOnce(consoleErrorStub);
+            sinon.assert.calledWith(consoleErrorStub, sinon.match.string, sinon.match.instanceOf(Error));
         });
     });
 
